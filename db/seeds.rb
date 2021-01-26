7f555d33a29a0346081cbebe98b4fba3780f7ddf
divyams1 <divyamsat@gmail.com>
2021-01-26 10:08:43 -0500

    seed data typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ require 'open-uri'
 
 Video.destroy_all 
 Genre.destroy_all
-GenreVideos.destroy_all
+GenresVideo.destroy_all
 
 
 video1 = Video.create(title: "Guardians of the Galaxy",  description:

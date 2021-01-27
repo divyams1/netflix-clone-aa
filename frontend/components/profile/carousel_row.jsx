ceaf7350787f65b5312ea422450190a74d2aa0ba
divyams1 <divyamsat@gmail.com>
2021-01-27 13:54:44 -0500

    added all videos and genres

--- frontend/components/profile/carousel_row.jsx
+++ frontend/components/profile/carousel_row.jsx
@@ -28,7 +28,7 @@ class CarouselRow extends React.Component {
         const profileVideos = profile.videos
         const right = <FontAwesomeIcon icon={faChevronRight} size={'3x'} />
         const left = <FontAwesomeIcon icon={faChevronLeft} />
-        const videos_to_render = this.state.videos.slice(0,1);
+        const videos_to_render = this.state.videos.slice(0,6);
       
         const videos = videos_to_render.map( (video, idx) => {
           
